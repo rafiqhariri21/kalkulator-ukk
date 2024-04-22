bfc9094ce41a182ee1038ef3e2a08652d476ff02
Rafiq Hariri <rafiqhariri011@gmail.com>
2024-04-22 22:44:21 +0700

    first commit

---
+++ app/Http/Controllers/ProsesController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProsesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $a =$request ->a;
+        $b =$request ->b;
+        $op =$request ->op;
+
+        if($op=='Tambah'){
+            $hasil =$a+$b;
+        }elseif($op=='Kurang'){
+            $hasil =$a-$b;
+        }elseif($op=='Kali'){
+            $hasil =$a*$b;
+        }elseif($op=='Reset'){
+            $a=0;
+            $b=0;
+            $hasil=0;
+        }elseif($op=='Bagi'){
+            if ($b !=0){
+                $hasil=$a/$b;
+            }else{
+                $hasil="Tidak Dapat Melakukan Pembagian Dengan 0!";
+            }
+        }
+        return view('kalkulator.hasil', compact('hasil'));
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/kalkulator/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: grey;
+            color: green;
+            padding: 10px;
+            font-family: "ALGERIAN";
+            margin: 5px;
+        }
+        .calculator-buttons input{
+            background-color: #776B5D;
+            color: light blue;
+            padding: 5px;
+            margin: 5px;
+            width: 100px;
+            height: 50px;
+            font-size: 20px;
+            font-style: thick;
+            box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.8);
+        }
+    </style>
+</head>
+<body>
+    <div class="calculator">
+        <center><h1>KALKULATOR SEDERHANA</h1>
+    <form action="{{route('proses.store')}}" method="post">
+        @csrf
+        <label for="a">Masukkan Angka Pertama</label><br>
+        <input type="number" name="a" id="a"><br><br>
+        <label for="b">Masukkan Angka Kedua</label><br>
+        <input type="number" name="b" id="b"><br><br>
+
+        <div class="calculator-buttons input">
+            <input type="submit" value="Kali" name="op">
+            <input type="submit" value="Bagi" name="op">
+            <input type="submit" value="Tambah" name="op">
+            <input type="submit" value="Kurang" name="op">            
+            <input type="submit" value="Reset" name="op">            
+
+        </div>
+    </form></center>
+    </div>
+</body>
+</html>
